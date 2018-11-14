32ff124e3e33df1fc3a764bb293797ed178e1882
Sedlyarskiy <ya.halo-halo@yandex.ru>
2018-11-14 23:42:44 +0400

    Транслятор. Генерирует код с переменными и константными строками

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace Translator
     {
         static void Main(string[] args)
         {
+            Vocabulary v = new Vocabulary();
+
+            StreamReader sr = new StreamReader("input.pas");
+            var inputCode = sr.ReadToEnd();
+            sr.Close();
+
+            StreamReader headerStream = new StreamReader("header.txt");
+            StreamReader bottomStream = new StreamReader("bottom.txt");
+
+            string header = headerStream.ReadToEnd();
+            string bottom = bottomStream.ReadToEnd();
+
+            headerStream.Close();
+            bottomStream.Close();
+            
+            var variables = v.GetVariables(inputCode);
+            var strings = v.GetConstantString(inputCode);
+
+            StreamWriter sw = new StreamWriter("output.asm");            
+
+            sw.WriteLine(header);
+
+            foreach (var i in variables)
+            {
+                sw.WriteLine(i + " dd ?");
+            }
+
+            foreach (var i in strings)
+            {
+                sw.WriteLine(i.Value + " db '" + i.Key + "',0");
+            }
+
+            sw.WriteLine(bottom);
+
+            sw.Close();
         }
     }
 }
---
+++ Translator/Vocabulary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Translator
+{
+    class Vocabulary
+    {
+
+        public HashSet<string> GetVariables(string code)
+        {
+            var result = new HashSet<string>();
+
+            int startVarBlock = code.IndexOf("var");
+            int endVarBlock = code.IndexOf("begin");
+
+            string varBlock = code.Substring(startVarBlock + 3, endVarBlock - (startVarBlock + 3) );
+            varBlock = varBlock.Trim();
+
+            string[] declarationLines = varBlock.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < declarationLines.Length; i++)
+            {
+                if (!declarationLines[i].Contains("integer"))
+                {
+                    throw new Exception("Data type expected!");                    
+                }
+
+                //int startTypeWord = declarationLines.
+                //declarationLines[i].Remove(0, "integer".Length);
+                declarationLines[i] = declarationLines[i].Substring(0, declarationLines[i].Length - "integer".Length);
+                declarationLines[i] = declarationLines[i].Trim();
+                declarationLines[i] = declarationLines[i].Replace(":", "");
+
+                var variables = declarationLines[i].Split(',');
+                foreach (var j in variables)
+                {
+                    //result.Add(j.Trim());
+                    var tmp = j.Trim();
+                    if (result.Contains(j))
+                    {
+                        throw new Exception("name already in use - " + tmp);
+                    }
+
+                    result.Add(tmp);
+                }
+            }
+
+            return result;
+        }
+
+        public Dictionary<string, string> GetConstantString(string code)
+        {
+            var result = new Dictionary<string, string>();
+            
+            var pattern = @"('[^']*')";
+            Regex reg = new Regex(pattern);
+
+            var matches = reg.Matches(code);
+            
+            for (int i = 0; i < matches.Count; i++)
+            {
+                if (!result.ContainsKey(matches[i].Value))
+                {
+                    var str = matches[i].Value.Replace("'", "");
+                    result.Add(str, "string" + i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
